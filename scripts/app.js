b0ea06ea4a41209fb895aad9bb20e44b0d60752c
Ryan Mallampati <ryanmallampati@gmail.com>
2024-04-28 18:30:51 +1000

    CAPS-81 Cursor Particle Effect Implementation

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,10 @@ function subscribe() {
     var email = document.querySelector('.subscribe-bar input').value;
     alert('Thank you for subscribing, ' + email + '!');
 }
+
+
+window.addEventListener("load", (event) => {
+  new cursoreffects.fairyDustCursor({
+  colors: ["#8e7cc3", "#ffd966", "#969490"],
+});
+});
